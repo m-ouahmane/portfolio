8e92af907519537dc1e75320e3de814d733cc716
m-ouahmane <60740775+m-ouahmane@users.noreply.github.com>
2020-07-08 20:36:11 +0200

    Add files via upload

---
+++ projects.php
@@ -0,0 +1,39 @@
+<head>
+	<title>Projects</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style/stl.css">
+	<link rel="stylesheet" type="text/css" href="style/stlProjs.css">
+	<nav>
+		<img src="media/logo.png">
+		<ul><li><a href="index.html">Home</a></li></ul>
+		<ul><li><a href="projects.php">Projects</a></li></ul>
+		<ul><li><a href="contact.php">Contact</a></li></ul>
+		<ul><li><a href="about.html">About</a></li></ul>
+	</nav>
+</head>
+<?php 
+
+?>
+<div class="footer">
+	<div class="pages">
+	<p>
+		<h2>Go to</h2>
+		<a href="index.html">Home</a><br>
+		<a href="projects.php">Projects</a><br>
+		<a href="about.html">About</a><br>
+	</p>
+	</div>
+	<div class="copyright">
+		<p>
+			<img src="media/logo.png">
+			<h3>SM-OUAHMANE&copy;2020</h3>
+		</p>
+	</div>
+	<div class="utilisation">
+		<p>
+			<h2>Check</h2>
+			<a href="contact.html">Contact</a><br>
+			<a href="legal.html">Mentions légales</a>
+		</p>
+	</div>
+</div>
